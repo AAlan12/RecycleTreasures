78e6402b407cda6c8ca6993fd23132394b93692f
Antonio Alan <tonio.alan07@gmail.com>
2023-06-05 09:59:39 -0300

    criação Products components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Hero } from '../../components/Hero'
+import { Products } from '../../components/Products'
 
 export const Home = () => {
-  return (
-    <Hero />
-  )
+    return (
+        <>
+            <Hero />
+            <h2 className=' text-4xl font-bold text-center mt-20'>Produtos</h2>
+            <Products />
+        </>
+    )
 }
