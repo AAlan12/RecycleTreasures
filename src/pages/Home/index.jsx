79dfbe552f00afb10bbd5cc9c8ebb9596a45c38a
Antonio Alan <tonio.alan07@gmail.com>
2023-06-06 08:53:58 -0300

    criação FeatureCard component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Hero } from '../../components/Hero'
 import { Products } from '../../components/Products'
+import { FeatureCard } from '../../components/FeatureCard'
 
 export const Home = () => {
     return (
         <>
             <Hero />
-            <h2 className=' text-4xl font-bold text-center mt-20'>Produtos</h2>
+            <div className="flex flex-col text-center w-full mt-10">
+                <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Produtos</h2>
+                <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">MAIS POPULARES</h1>
+            </div>
             <Products />
+            <FeatureCard />
         </>
     )
 }
