81d21039ec0b83b2257d441211280ea7284eb147
Antonio Alan <tonio.alan07@gmail.com>
2023-06-06 09:07:05 -0300

    criação do Footer component

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { Header } from './components/Header'
 import { Home } from './pages/Home'
+import { Footer } from './components/Footer'
 
 export const Router = () => {
   return (
     <BrowserRouter>
         <Header />
         <Home />
+        <Footer />
     </BrowserRouter>
   )
 }
