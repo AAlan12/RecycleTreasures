0317b9d441e7e97cc5fb68c8541d34855b786657
Antonio Alan <tonio.alan07@gmail.com>
2023-06-07 01:00:20 -0300

    dados da api na Products component

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -7,15 +7,16 @@ export const Products = ({ products = [] }) => {
                 <div className="flex flex-wrap -m-4">
                     {
                         products.map((product) => {
+                            const { id, title, price, description, category, image } = product;
                             return (
-                                <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                <div className="lg:w-1/4 md:w-1/2 p-4 w-full border border-opacity-50">
                                     <a className="block relative h-48 rounded overflow-hidden">
-                                        <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
+                                        <img alt={title} className="object-contain object-center w-full h-full block" src={image} />
                                     </a>
                                     <div className="mt-4">
-                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                        <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                                        <p className="mt-1">$16.00</p>
+                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1 uppercase">{category}</h3>
+                                        <h2 className="text-gray-900 title-font text-lg font-medium">{title}</h2>
+                                        <p className="mt-1 text-md font-semibold">{price}</p>
                                     </div>
                                 </div>
                             )
