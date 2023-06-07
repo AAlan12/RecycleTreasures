d8265bacac77055ad8ec8a4fe547ebd8120356c9
Antonio Alan <tonio.alan07@gmail.com>
2023-06-07 08:51:42 -0300

    criação Products page

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Header } from './components/Header'
 import { Home } from './pages/Home'
 import { Footer } from './components/Footer'
+import { Product } from './pages/Product'
 
 export const Router = () => {
   return (
@@ -9,6 +10,7 @@ export const Router = () => {
         <Header />
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/products/:id" element={<Product />} />
         </Routes>
         <Footer />
     </BrowserRouter>
