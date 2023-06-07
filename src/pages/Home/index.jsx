b356c5bbb8ef9199feebccf2c351faaca1eb73ae
Antonio Alan <tonio.alan07@gmail.com>
2023-06-07 00:50:13 -0300

    add api fake store api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { Hero } from '../../components/Hero'
 import { Products } from '../../components/Products'
 import { FeatureCard } from '../../components/FeatureCard'
 import { StatCard } from '../../components/StatCard'
 
 export const Home = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await fetch ('https://fakestoreapi.com/products')
+            const data = await response.json()
+            console.log(data)
+            setProducts(data)
+        }
+        fetchProducts()
+    }, [])
+
     return (
         <>
             <Hero />
@@ -12,6 +23,12 @@ export const Home = () => {
                 <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Produtos</h2>
                 <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">MAIS POPULARES</h1>
             </div>
+            {
+                products.length > 0 ?
+                <Products products={products} />
+                :
+                <div>Loading...</div>
+            }
             <Products />
             <FeatureCard />
             <StatCard />
